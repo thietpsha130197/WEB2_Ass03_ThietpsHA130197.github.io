5e8fe888ece14475fad365d93545f3a21298f6ed
thietpsha130197 <81202940+thietpsha130197@users.noreply.github.com>
2021-04-01 12:55:14 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# thietpsha130197-WEB2_Ass03_ThietpsHA130197.github.io
---
+++ js/project.js
@@ -0,0 +1,60 @@
+(function($) { "use strict";
+    
+  //Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+    t.style.top = n.clientY + "px", 
+    e.style.left = n.clientX + "px", 
+    e.style.top = n.clientY + "px", 
+    i.style.left = n.clientX + "px", 
+    i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+  
+  //About page
+  
+  $(".about-text").on('click', function () {
+    $("body").addClass("about-on");
+  });
+  $(".about-close").on('click', function () {
+    $("body").removeClass("about-on");
+  });
+
+  
+  //Contact page
+  
+  $(".contact-text").on('click', function () {
+    $("body").addClass("contact-on");
+  });
+  $(".contact-close").on('click', function () {
+    $("body").removeClass("contact-on");
+  });
+  
+  
+  //Work portfolio page
+  
+  $(".Work").on('click', function () {
+    $("body").addClass("Work-on");
+  });
+  $(".Work-close").on('click', function () {
+    $("body").removeClass("Work-on");
+  });
+  
+})(jQuery); 
